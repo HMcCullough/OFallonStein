7d6fd7efc67e5e1e0708b8c58a09b09a740bde48
Hunter McCullough <huntermccullough56@gmail.com>
2017-04-23 21:20:05 -0700

    Added partial specialization for list class so that when it contains pointers they are freed appropriately

--- E_Def.h
+++ E_Def.h
@@ -11,6 +11,7 @@
 #include <SDL/SDL_mixer.h>
 #include <climits>
 #include <random>
+#include <type_traits>
 
 #include "HelperClasses/Vector2.h"
 #include "quickcg.h"
--- HelperClasses/List.h
+++ HelperClasses/List.h
@@ -13,6 +13,11 @@ public:
 		mCount = 0;
 	}
 
+	~List<T>()
+	{
+		delete mpHead;
+	}
+
 	int size() const { return mCount; }
 
 	Node<T> *getHead() const { return mpHead; }
@@ -96,6 +101,7 @@ public:
 			mpHead->setNext(nullptr);
 			newHead->setPrev(nullptr);
 		}
+
 		delete mpHead;
 
 		mpHead = newHead;
@@ -118,6 +124,7 @@ public:
 			mpTail->setPrev(nullptr);
 			newTail->setNext(nullptr);
 		}
+
 		delete mpTail;
 
 		mpTail = newTail;
@@ -153,8 +160,8 @@ public:
 		nextNode->setPrev(prevNode);
 
 		delete curNode;
-		mCount--;
 
+		mCount--;
 		return true;
 	}
 
@@ -187,3 +194,205 @@ private:
 			*mpTail;
 	int mCount;
 };
+
+template <class T>
+class List<T*>
+{
+public:
+	List<T*>()
+	{
+		mpHead = nullptr;
+		mpTail = nullptr;
+		mCount = 0;
+	}
+
+	~List<T*>()
+	{
+		for (Node<T*> *cur = mpHead; cur != nullptr; cur = cur->getNext())
+			delete cur->getData();
+		delete mpHead;
+	}
+
+	int size() const { return mCount; }
+
+	Node<T*> *getHead() const { return mpHead; }
+
+	void print()
+	{
+		Node<T*> *item = mpHead;
+
+		while (item != nullptr)
+		{
+			std::cout << item->getData() << " ";
+			item = item->getNext();
+		}
+
+		std::cout << std::endl;
+	}
+
+	void printReverse()
+	{
+		Node<T*> *item = mpTail;
+
+		while (item != nullptr)
+		{
+			std::cout << item->getData() << " ";
+			item = item->getPrev();
+		}
+
+		std::cout << std::endl;
+	}
+
+	bool isEmpty()
+	{
+		return mpHead == nullptr;
+	}
+
+	void insertAtFront(T data)
+	{
+		Node<T*> *newHead = new Node<T*>(data);
+
+		if (mpHead == nullptr)
+		{
+			mpTail = newHead;
+		}
+		else
+		{
+			newHead->setNext(mpHead);
+			mpHead->setPrev(newHead);
+		}
+		mCount++;
+
+		mpHead = newHead;
+	}
+
+	void insertAtEnd(T data)
+	{
+		Node<T*> *newTail = new Node<T*>(data);
+
+		if (mpTail == nullptr)
+		{
+			mpHead = newTail;
+		}
+		else
+		{
+			newTail->setPrev(mpTail);
+			mpTail->setNext(newTail);
+		}
+		mCount++;
+
+		mpTail = newTail;
+	}
+
+	bool deleteFromFront()
+	{
+		Node<T*> *newHead = mpHead->getNext();
+
+		if (mpHead == nullptr)
+			return false;
+
+		if (newHead != nullptr)
+		{
+			mpHead->setNext(nullptr);
+			newHead->setPrev(nullptr);
+		}
+
+		if (std::is_pointer<T*>::value)
+			delete mpHead->getData();
+
+		delete mpHead;
+
+		mpHead = newHead;
+		if (mpHead == nullptr)
+			mpTail = newHead;
+		mCount--;
+
+		return true;
+	}
+
+	bool deleteFromEnd()
+	{
+		Node<T*> *newTail = mpTail->getPrev();
+
+		if (mpTail == nullptr)
+			return false;
+
+		if (newTail != nullptr)
+		{
+			mpTail->setPrev(nullptr);
+			newTail->setNext(nullptr);
+		}
+
+		delete mpHead->getData();
+
+		delete mpTail;
+
+		mpTail = newTail;
+		if (mpTail == nullptr)
+			mpHead = newTail;
+		mCount--;
+
+		return true;
+	}
+
+	bool deleteAt(int index)
+	{
+		Node<T*> *curNode = mpHead, *prevNode, *nextNode;
+
+		for (int i = 0; i < index && curNode != nullptr; ++i)
+			curNode = curNode->getNext();
+
+		if (curNode == nullptr)
+			return false;
+
+		prevNode = curNode->getPrev();
+		nextNode = curNode->getNext();
+
+		if (prevNode == nullptr)
+			return deleteFromFront();
+		else if (nextNode == nullptr)
+			return deleteFromEnd();
+
+		curNode->setNext(nullptr);
+		curNode->setPrev(nullptr);
+
+		prevNode->setNext(nextNode);
+		nextNode->setPrev(prevNode);
+
+		delete mpHead->getData();
+		delete curNode;
+
+		mCount--;
+		return true;
+	}
+
+	T &at(int index)
+	{
+		Node<T*> *curNode = mpHead;
+		for (int i = 0; i < index && curNode != nullptr; ++i)
+			curNode = curNode->getNext();
+
+		if (curNode == nullptr)
+		{
+			std::string ex = "Data does not exist at index " + std::to_string(index);
+			throw std::out_of_range(ex);
+		}
+
+		return curNode->getData();
+	}
+
+	T &operator[](int index)
+	{
+		Node<T*> *curNode = mpHead;
+		for (int i = 0; i < index && curNode != nullptr; ++i)
+			curNode = curNode->getNext();
+
+		return curNode->getData();
+	}
+
+private:
+	Node<T*> *mpHead,
+			 *mpTail;
+	int mCount;
+};
+
